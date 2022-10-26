f5cf59eccea40864fe1c99a88b9a2b9bce12cb51
raylu15742cs <raylucs15742@gmail.com>
2022-10-25 19:16:46 -0700

    update project

--- portfolio/components/projectcomponent.tsx
+++ portfolio/components/projectcomponent.tsx
@@ -1,7 +1,7 @@
 export default function Projectcomponent() {
     return (
          <div>
-            <h1 className='text-6xl'>This will showcase the projects</h1>
+            <h1 className='text-3xl'>This will showcase the projects</h1>
         </div>
     )
 }
