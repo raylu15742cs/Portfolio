5c3287636da769cb99c3e6a889696180790a5120
raylu15742cs <raylucs15742@gmail.com>
2022-10-26 14:41:30 -0700

    add profile image

--- portfolio/components/projectcomponent.tsx
+++ portfolio/components/projectcomponent.tsx
@@ -2,6 +2,9 @@ export default function Projectcomponent() {
     return (
          <div>
             <h1 className='text-3xl'>This will showcase the projects</h1>
+            <h2> Project 1 </h2>
+            <h2> Project 2 </h2>
+            <h2> Project 3 </h2>
         </div>
     )
 }
--- portfolio/pages/index.tsx
+++ portfolio/pages/index.tsx
@@ -2,14 +2,17 @@ import Header from './header'
 import Projectcomponent from '../components/projectcomponent'
 import Aboutcomponent from '../components/aboutcomponent'
 import Resumecomponent from '../components/resumecomponent'
+import Image from 'next/image'
+
 
 export default function Home() {
   return (
     <div className='bg-[#d7eaf3] text-[#14397d] h-screen '>
       <Header />
-      <h1 className='text-6xl'> This is Home</h1>
-      <Projectcomponent />
+      <h1 className='text-6xl'>Raymond Lu</h1>
+      <Image src="/images/profile.png" alt={''} width={500} height={500} />
       <Aboutcomponent />
+      <Projectcomponent />
       <Resumecomponent />
     </div>
   )
