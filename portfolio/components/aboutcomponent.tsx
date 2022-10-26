952f4a8892d3a6c5ee0342af4a20e831a89b419f
raylu15742cs <raylucs15742@gmail.com>
2022-10-25 19:14:05 -0700

    update about me

--- portfolio/components/aboutcomponent.tsx
+++ portfolio/components/aboutcomponent.tsx
@@ -1,7 +1,7 @@
 export default function Aboutcomponent() {
     return (
          <div>
-            <h1 className='text-6xl'>This is About</h1>
+            <h1 className='text-6xl'>This will be the about me</h1>
         </div>
     )
 }
