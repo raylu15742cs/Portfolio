bf993dc5348750491fc2b883cbe2b94ae9e9d3ff
raylu15742cs <raylucs15742@gmail.com>
2022-10-26 10:52:56 -0700

    add color to resume

--- portfolio/pages/resume.tsx
+++ portfolio/pages/resume.tsx
@@ -3,7 +3,7 @@ import Header from "./header"
 
 export default function Resume() {
     return (
-         <div>
+         <div className='bg-[#d7eaf3] text-[#14397d] h-screen  dark:bg-[#14397d] dark:text-[#d7eaf3]'>
             <Header />
             <Resumecomponent />
         </div>
