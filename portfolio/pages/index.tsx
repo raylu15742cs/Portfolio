c341ca9f3b0e3902b27dc6697532e4a8685c1f5b
raylu15742cs <raylucs15742@gmail.com>
2022-10-25 19:59:39 -0700

    update bg and text

--- portfolio/pages/index.tsx
+++ portfolio/pages/index.tsx
@@ -5,7 +5,7 @@ import Resumecomponent from '../components/resumecomponent'
 
 export default function Home() {
   return (
-    <div>
+    <div className='bg-[#FF7600] text-gray-200'>
       <Header />
       <h1 className='text-6xl'> This is Home</h1>
       <Projectcomponent />
