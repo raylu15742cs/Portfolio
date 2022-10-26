10736d02a41901d9eced0fa5b991eb9aade6cb77
raylu15742cs <raylucs15742@gmail.com>
2022-10-25 19:16:34 -0700

    update main

--- portfolio/components/maincomponent.tsx
+++ portfolio/components/maincomponent.tsx
@@ -1,7 +1,7 @@
 export default function Maincomponent() {
     return (
          <div>
-            <h1 className='text-6xl'>This is will be the main</h1>
+            <h1 className='text-3xl'>This is will be the main</h1>
         </div>
     )
 }
