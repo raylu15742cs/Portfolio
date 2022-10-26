0b70902db652998e76122e5f96051f207a2e0fcc
raylu15742cs <raylucs15742@gmail.com>
2022-10-26 10:30:41 -0700

    add dark mode

--- portfolio/pages/index.tsx
+++ portfolio/pages/index.tsx
@@ -5,7 +5,7 @@ import Resumecomponent from '../components/resumecomponent'
 
 export default function Home() {
   return (
-    <div className='bg-[#d7eaf3] text-[#14397d] h-screen'>
+    <div className='bg-[#d7eaf3] text-[#14397d] h-screen  dark:bg-[#14397d] dark:text-[#d7eaf3]'>
       <Header />
       <h1 className='text-6xl'> This is Home</h1>
       <Projectcomponent />
