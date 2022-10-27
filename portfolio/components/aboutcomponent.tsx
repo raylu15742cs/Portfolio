27f056889e96b716253790d665c444f0438df024
raylu15742cs <raylucs15742@gmail.com>
2022-10-27 14:09:59 -0700

    update timeline

--- portfolio/components/aboutcomponent.tsx
+++ portfolio/components/aboutcomponent.tsx
@@ -1,7 +1,25 @@
 export default function Aboutcomponent() {
     return (
-         <div className="text-center">
-            <h1 className='text-3xl'>About Me</h1>
+         <div>
+            <h1 className='text-3xl text-center'>My Life Story</h1>
+            <div className=" p-2 m-5 border-2 border-slate-900">
+                <h1 className="text-center text-2xl">2022</h1>
+                <li className="text-sm">Committed full time to becoming a developer</li>
+                <li className="text-sm">Created a Youtube Channel</li>
+            </div>
+            <div className=" p-2 m-5 border-2 border-slate-900">
+                <h1 className="text-center text-2xl">2021</h1>
+                <li className="text-sm">Awarded Bachelors of Arts in Economics</li>
+                <li className="text-sm">Took an Introduction to Computer Science Course</li>
+            </div>
+            <div className=" p-2 m-5 border-2 border-slate-900">
+                <h1 className="text-center text-2xl">2019</h1>
+                <li className="text-sm">Completed associates degree and transferred to University of California Irvine</li>
+            </div>
+            <div className="  p-2 m-5 border-2 border-slate-900">
+                <h1 className="text-center text-2xl">2017</h1>
+                <li className="text-sm"> As a Sophomore decided to take exit exam to start attending college</li>
+            </div>
         </div>
     )
 }
