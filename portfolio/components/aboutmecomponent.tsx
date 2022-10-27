f31e571d3e7c2266956e0b5cba1306fd9f9b9467
raylu15742cs <raylucs15742@gmail.com>
2022-10-27 16:36:35 -0700

    add spacing on about me

--- portfolio/components/aboutmecomponent.tsx
+++ portfolio/components/aboutmecomponent.tsx
@@ -4,7 +4,7 @@ export default function Aboutmecomponent() {
             <h1 className='text-3xl text-center'>About Me</h1>
             <div className="">
                 <p>Hello! My name is Raymond and I am trying to build a career in the world of web development. I have an economics background, which I value since it taught me to see the world in a logical way.</p>
-                <p>My first taste of web development was when I decided to take an intro to computer science course based on python. It was one of the courses that I will never forget, and it definitely peaked my interested in the field. </p>
+                <p className="pt-1">My first taste of web development was when I decided to take an intro to computer science course based on python. It was one of the courses that I will never forget, and it definitely peaked my interested in the field. </p>
             <p className="pt-1">Here are a few technologies that I currently am using: </p>
             <div className="flex pt-1">
                 <ul className="pr-8 pl-3 list-disc">
