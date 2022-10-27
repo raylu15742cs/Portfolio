8643badf1897cbc1b91af7aa42f6d6bd8078c564
raylu15742cs <raylucs15742@gmail.com>
2022-10-26 22:03:52 -0700

    remove bad videos

--- portfolio/components/projectcomponent.tsx
+++ portfolio/components/projectcomponent.tsx
@@ -4,7 +4,10 @@ export default function Projectcomponent() {
     return (
          <div className="text-center pt-4">
             <h1 className='text-3xl'>Projects</h1>
-            <h2> Project 1 </h2>
+            <h2> Yeezy Beluga </h2>
+            <video autoPlay muted loop >
+                <source src="/images/beluga.mp4" type="video/mp4" />
+            </video>
             <Image className="h-auto w-[90vw] ml-[auto] mr-[auto] pt-2" src="/images/rectangle.jpeg" alt={''} width={500} height={500} />
             <h2> Project 2 </h2>
             <Image className="h-auto w-[90vw] ml-[auto] mr-[auto] pt-2" src="/images/rectangle.jpeg" alt={''} width={500} height={500} />
