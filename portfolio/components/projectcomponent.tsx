bf32976b48660b6b961703240ef003e29f811304
raylu15742cs <raylucs15742@gmail.com>
2022-10-27 11:50:30 -0700

    remove title on mobile

--- portfolio/components/projectcomponent.tsx
+++ portfolio/components/projectcomponent.tsx
@@ -4,15 +4,12 @@ export default function Projectcomponent() {
     return (
          <div className="text-center pt-4">
             <h1 className='text-3xl'>Projects</h1>
-            <h2> Pokemon </h2>
             <video className="max-w-[80%] mx-auto border-2 border-slate-800" playsInline autoPlay muted loop >
                 <source src="/images/pokemon.mp4" type="video/mp4" />
             </video>
-            <h2> Yeezy Beluga </h2>
             <video className="max-w-[80%] mx-auto border-2 border-slate-800" playsInline autoPlay muted loop >
                 <source src="/images/beluga.mp4" type="video/mp4" />
             </video>
-            <h2> Battleship </h2>
             <video className="max-w-[80%] mx-auto border-2 border-slate-800" playsInline autoPlay muted loop >
                 <source src="/images/battleship.mp4" type="video/mp4" />
             </video>
