714355b65592f0393e812c302323f093c353338b
raylu15742cs <raylucs15742@gmail.com>
2022-10-26 22:28:38 -0700

    add image and positioning

--- portfolio/components/projectcomponent.tsx
+++ portfolio/components/projectcomponent.tsx
@@ -5,15 +5,15 @@ export default function Projectcomponent() {
          <div className="text-center pt-4">
             <h1 className='text-3xl'>Projects</h1>
             <h2> Yeezy Beluga </h2>
-            <video className="" autoPlay muted loop >
+            <video className="w-4/6 mx-auto" autoPlay muted loop >
                 <source src="/images/beluga.mp4" type="video/mp4" />
             </video>
-            <h2> Project 2 </h2>
-            <video className="" autoPlay muted loop >
+            <h2> Pokemon </h2>
+            <video className="w-4/6  mx-auto" autoPlay muted loop >
                 <source src="/images/pokemon.mp4" type="video/mp4" />
             </video>
-            <h2> Project 3 </h2>
-            <video className="" autoPlay muted loop >
+            <h2> Battleship </h2>
+            <video className="w-4/6  mx-auto" autoPlay muted loop >
                 <source src="/images/battleship.mp4" type="video/mp4" />
             </video>
         </div>
