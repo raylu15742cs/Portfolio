dd572f39570781c5ea1bd71f4aaaa04de3242009
raylu15742cs <raylucs15742@gmail.com>
2022-10-27 16:15:02 -0700

    update project width

--- portfolio/components/projectcomponent.tsx
+++ portfolio/components/projectcomponent.tsx
@@ -4,7 +4,7 @@ export default function Projectcomponent() {
     return (
          <div className="text-center pt-4">
             <h1 className='text-3xl'>Projects</h1>
-            <video className="max-w-[80%] mx-auto border-2 border-slate-800 mt-2 rounded-lg" playsInline autoPlay muted loop >
+            <video className="max-w-[90%] mx-auto border-2 border-slate-800 mt-2 rounded-lg" playsInline autoPlay muted loop >
                 <source src="/images/pokemon.mp4" type="video/mp4" />
             </video>
             <video className="max-w-[80%] mx-auto border-2 border-slate-800 mt-2 rounded-lg" playsInline autoPlay muted loop >
