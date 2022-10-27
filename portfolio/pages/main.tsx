72a8f6998caa53ab0ff352391dc1b76ac4bc77e2
raylu15742cs <raylucs15742@gmail.com>
2022-10-27 12:10:20 -0700

    update main color theme

--- portfolio/pages/main.tsx
+++ portfolio/pages/main.tsx
@@ -2,7 +2,7 @@ import Header from "./header"
 import Maincomponent from "../components/maincomponent"
 export default function Main() {
     return (
-         <div className='bg-[#d7eaf3] text-[#14397d] h-screen  dark:bg-[#14397d] dark:text-[#d7eaf3]'> 
+         <div className='bg-[#e3e3e3] text-gray-700 pt-2 pb-5'> 
           <Header />
           <Maincomponent />
          </div>
