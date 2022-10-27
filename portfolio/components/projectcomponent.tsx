6e830450585916b0195761a4371a4186cc3a775d
raylu15742cs <raylucs15742@gmail.com>
2022-10-26 17:49:31 -0700

    add projects

--- portfolio/components/projectcomponent.tsx
+++ portfolio/components/projectcomponent.tsx
@@ -1,10 +1,15 @@
+import Image from "next/image"
+
 export default function Projectcomponent() {
     return (
-         <div>
-            <h1 className='text-3xl'>This will showcase the projects</h1>
+         <div className="text-center pt-4">
+            <h1 className='text-3xl'>Projects</h1>
             <h2> Project 1 </h2>
+            <Image className="h-auto w-[90vw] ml-[auto] mr-[auto] pt-2" src="/images/rectangle.jpeg" alt={''} width={500} height={500} />
             <h2> Project 2 </h2>
+            <Image className="h-auto w-[90vw] ml-[auto] mr-[auto] pt-2" src="/images/rectangle.jpeg" alt={''} width={500} height={500} />
             <h2> Project 3 </h2>
+            <Image className="h-auto w-[90vw] ml-[auto] mr-[auto] pt-2" src="/images/rectangle.jpeg" alt={''} width={500} height={500} />
         </div>
     )
 }
--- portfolio/pages/index.tsx
+++ portfolio/pages/index.tsx
@@ -7,17 +7,17 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className='bg-[#e3e3e3] text-gray-700 h-screen w-screen pt-1'>
+    <div className='bg-[#e3e3e3] text-gray-700 pt-2 pb-5'>
       <Header />
       <div className='text-center'>
         <div>
-          <Image className="h-1/8 w-auto pl-20 pt-2" src="/images/profile.png" alt={''} width={500} height={500} />
+          <Image className="h-[60vw] w-auto ml-[auto] mr-[auto] pt-2" src="/images/profile.png" alt={''} width={500} height={500} />
           <h1 className='text-5xl pt-1'>Raymond Lu</h1>  
           <p className='text-sm pt-1'>Front End Developer based in Pasadena, CA</p>
         </div>
       </div>
-      {/*       <Aboutcomponent />
       <Projectcomponent />
+      {/*       <Aboutcomponent />
       <Resumecomponent />
        */}
     </div>
