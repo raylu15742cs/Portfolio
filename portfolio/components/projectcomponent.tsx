701a885aeb14e86d371ef617412a398d5a226bb9
raylu15742cs <raylucs15742@gmail.com>
2022-10-27 11:51:00 -0700

    add margin top to video

--- portfolio/components/projectcomponent.tsx
+++ portfolio/components/projectcomponent.tsx
@@ -4,13 +4,13 @@ export default function Projectcomponent() {
     return (
          <div className="text-center pt-4">
             <h1 className='text-3xl'>Projects</h1>
-            <video className="max-w-[80%] mx-auto border-2 border-slate-800" playsInline autoPlay muted loop >
+            <video className="max-w-[80%] mx-auto border-2 border-slate-800 mt-2" playsInline autoPlay muted loop >
                 <source src="/images/pokemon.mp4" type="video/mp4" />
             </video>
-            <video className="max-w-[80%] mx-auto border-2 border-slate-800" playsInline autoPlay muted loop >
+            <video className="max-w-[80%] mx-auto border-2 border-slate-800 mt-2" playsInline autoPlay muted loop >
                 <source src="/images/beluga.mp4" type="video/mp4" />
             </video>
-            <video className="max-w-[80%] mx-auto border-2 border-slate-800" playsInline autoPlay muted loop >
+            <video className="max-w-[80%] mx-auto border-2 border-slate-800 mt-2" playsInline autoPlay muted loop >
                 <source src="/images/battleship.mp4" type="video/mp4" />
             </video>
         </div>
