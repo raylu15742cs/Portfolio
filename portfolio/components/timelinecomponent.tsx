bd7860a046a235920a3e43faaed519e3d2176980
raylu15742cs <raylucs15742@gmail.com>
2022-10-27 15:39:55 -0700

    update timeline

--- portfolio/components/timelinecomponent.tsx
+++ portfolio/components/timelinecomponent.tsx
@@ -2,7 +2,7 @@ export default function Timelinecomponent() {
     return (
          <div>
             <h1 className='text-3xl text-center'>My Life Story</h1>
-            <div className="flex p-2 mx-[10%] mt-2 border-2 border-slate-900">
+            <div className="flex p-2 mx-[10%] mt-2 border-2 border-slate-900 rounded-lg">
                 <div className=" text-xs">
                     <h1>2</h1>
                     <h1>0</h1>
