b8724f76dfa9b54abea3d8e76398095fc853dab8
raylu15742cs <raylucs15742@gmail.com>
2022-10-28 13:08:05 -0700

    add link to images

--- portfolio/components/connectcomponent.tsx
+++ portfolio/components/connectcomponent.tsx
@@ -7,11 +7,19 @@ export default function Connectcomponent() {
             <div className="text-center text-3xl">
                 Connect With Me
             </div>
-            <div className="flex mx-[10%]  ">
-                <Image className="h-[15vw] w-auto mx-auto " src="/images/youtubeicon.png" alt={''} width={500} height={500} />
-                <Image className="h-[15vw] w-auto mx-auto" src="/images/linkedin.png" alt={''} width={500} height={500} />
-                <Image className="h-[15vw] w-auto mx-auto" src="/images/githubicon.png" alt={''} width={500} height={500} />
-                <Image className="h-[15vw] w-auto mx-auto" src="/images/twittericon.png" alt={''} width={500} height={500} />
+            <div className="flex mx-[15%] pt-2 ">
+                <a className="mx-auto" href="https://github.com/raylu15742cs" target="#">
+                    <Image className="h-[40px] w-auto" src="/images/linkedin.png" alt={''} width={500} height={500} />
+                </a>
+                <a className="mx-auto"  href="hhttps://www.youtube.com/channel/UCZ0lcRhWFgwx0x7qsR-BlsQ" target="#">
+                    <Image className="h-[40px] w-auto  " src="/images/youtubeicon.png" alt={''} width={500} height={500} />
+                </a>
+                 <a className="mx-auto"  href="https://www.linkedin.com/in/raylu15742/" target="#">
+                    <Image className="h-[40px] w-auto" src="/images/githubicon.png" alt={''} width={500} height={500} />
+                </a>
+                <a className="mx-auto"  href="https://twitter.com/RaymondLuTech" target="#">
+                    <Image className="h-[45px] w-auto" src="/images/twittericon.png" alt={''} width={500} height={500} />
+                </a>
             </div>
         </div>
     )
