16ee0ff08c80fc354d6319a1bfb4b61941d81fb8
raylu15742cs <raylucs15742@gmail.com>
2022-10-28 15:13:07 -0700

    update main

--- portfolio/pages/main.tsx
+++ portfolio/pages/main.tsx
@@ -2,7 +2,7 @@ import Header from "./header"
 import Maincomponent from "../components/aboutmecomponent"
 export default function Main() {
     return (
-         <div className='bg-[#e3e3e3] text-gray-700 pt-2 pb-5'> 
+         <div className='bg-[#e3e3e3] text-gray-700 pt-2 pb-5 h-screen'> 
           <Header />
           <Maincomponent />
          </div>
