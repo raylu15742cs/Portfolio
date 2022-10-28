5e5436aa36d34663521085bc70ae840ede41b4d0
raylu15742cs <raylucs15742@gmail.com>
2022-10-28 14:22:31 -0700

    update responsive

--- portfolio/pages/index.tsx
+++ portfolio/pages/index.tsx
@@ -10,12 +10,19 @@ export default function Home() {
   return (
     <div className='bg-[#e3e3e3] text-gray-700 pt-2 pb-2'>
       <Header />
-      <div className='text-center'>
-        <div>
-          <Image className="h-[40vw] w-auto ml-[auto] mr-[auto] pt-2" src="/images/profile.png" alt={''} width={500} height={500} />
-          <h1 className='text-5xl pt-1'>Raymond Lu</h1>  
+      <div className='text-center '>
+        <div className='sm:flex items-center md:mx-auto'>
+          <div className='hidden sm:inline mx-auto sm:mr-0'>
+            <h1 className='text-5xl pt-1'>Raymond Lu</h1>  
           <p className='text-sm pt-1'>Front End Developer based in Pasadena, CA</p>
           <p>求不得 放不下</p>
+          </div>
+          <Image className="h-[30vh] w-auto pt-2 mx-auto sm:ml-0" src="/images/profile.png" alt={''} width={500} height={500} />
+          <div className='sm:hidden'>
+            <h1 className='text-5xl pt-1'>Raymond Lu</h1>  
+          <p className='text-sm pt-1'>Front End Developer based in Pasadena, CA</p>
+          <p>求不得 放不下</p>
+          </div>
         </div>
       </div>
       <Projectcomponent />
