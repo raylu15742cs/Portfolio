a4e19bdbf7b5c8ff4e8309a5c9b136486161ac6b
raylu15742cs <raylucs15742@gmail.com>
2022-10-28 12:56:48 -0700

    add github icon

--- portfolio/components/connectcomponent.tsx
+++ portfolio/components/connectcomponent.tsx
@@ -10,7 +10,7 @@ export default function Connectcomponent() {
             <div className="flex mx-[10%]  ">
                 <Image className="h-[15vw] w-auto mx-auto " src="/images/youtubeicon.png" alt={''} width={500} height={500} />
                 <Image className="h-[15vw] w-auto mx-auto" src="/images/linkedin.png" alt={''} width={500} height={500} />
-                <Image className="h-[15vw] w-auto mx-auto" src="/images/youtubeicon.png" alt={''} width={500} height={500} />
+                <Image className="h-[15vw] w-auto mx-auto" src="/images/githubicon.png" alt={''} width={500} height={500} />
                 <Image className="h-[15vw] w-auto mx-auto" src="/images/youtubeicon.png" alt={''} width={500} height={500} />
             </div>
         </div>
