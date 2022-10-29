9fd0b5c60a3e7f5cf2ae2dff021953023089c858
raylu15742cs <raylucs15742@gmail.com>
2022-10-28 23:28:06 -0700

    update about me

--- portfolio/components/aboutmecomponent.tsx
+++ portfolio/components/aboutmecomponent.tsx
@@ -4,7 +4,7 @@ export default function Aboutmecomponent() {
             <h1 className='text-3xl text-center'>About Me</h1>
             <div className="">
                 <p>Hello! My name is Raymond and I am trying to build a career in the world of web development. I have an economics background, which I value since it taught me to see the world in a logical way.</p>
-                <p className="pt-1">My first taste of web development was when I decided to take an intro to computer science course based on python. It was one of the courses that I will never forget, and it definitely peaked my interested in the field. </p>
+                <p className="pt-1">My first taste of web development was when I decided to take an intro to computer science course based on python. It was one of the courses that I will never forget, and it definitely peaked my interest in the field. </p>
                 <p className="pt-1">I decided to take the next step and start learning web development on my own. I started with the Odin Project, and I am currently working on the curriculum. I am also working on a Youtube channel where I will be posting videos on my journey. </p>
             <p className="pt-1">Here are a few technologies that I currently am using: </p>
             <div className="flex pt-1">
