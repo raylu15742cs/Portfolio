1003f6699579773b6ff10c9660aecfe31b182ea0
raylu15742cs <raylucs15742@gmail.com>
2022-11-01 09:16:40 -0700

    change title of project

--- portfolio/components/projectcomponent.tsx
+++ portfolio/components/projectcomponent.tsx
@@ -22,7 +22,7 @@ export default function Projectcomponent() {
                     </video>
                 </div>
                 <div>
-                    <h1 className='text-3xl'>Pokemon Memory Game</h1>
+                    <h1 className='text-3xl'>Battleship Replica</h1>
                     <video className="max-w-[90%] mx-auto border-2 border-slate-800 mt-2 rounded-lg md:w-[48%]" playsInline autoPlay muted loop >
                         <source src="/images/battleship.mp4" type="video/mp4" />
                     </video>
