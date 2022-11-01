b60cbb282ca773f263e7d999dea52a2af67f928e
raylu15742cs <raylucs15742@gmail.com>
2022-11-01 08:49:12 -0700

    remov xl on project

--- portfolio/components/projectcomponent.tsx
+++ portfolio/components/projectcomponent.tsx
@@ -3,7 +3,7 @@ export default function Projectcomponent() {
          <div className=" text-center pt-4 lg:mx-[5%]">
             <h1 className='text-3xl'>Most Recent Projects</h1>
             <div className="flex flex-wrap">
-            <video className="max-w-[90%] mx-auto border-2 border-slate-800 mt-2 rounded-lg md:w-[48%] xl:w-[33%]" playsInline autoPlay muted loop >
+            <video className="max-w-[90%] mx-auto border-2 border-slate-800 mt-2 rounded-lg md:w-[48%]" playsInline autoPlay muted loop >
                 <source src="/images/pokemon.mp4" type="video/mp4" />
             </video>
             <video className="max-w-[90%] mx-auto border-2 border-slate-800 mt-2 rounded-lg md:w-[48%]  xl:w-[33%]" playsInline autoPlay muted loop >
