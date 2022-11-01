1c85ceedb1afac9413f20e4320ea4a4ab835da72
raylu15742cs <raylucs15742@gmail.com>
2022-11-01 09:14:43 -0700

    add new project

--- portfolio/components/projectcomponent.tsx
+++ portfolio/components/projectcomponent.tsx
@@ -18,12 +18,10 @@ export default function Projectcomponent() {
                 <div>
                     <h1 className='text-3xl'>Pokemon Memory Game</h1>
                     <video className="max-w-[90%] mx-auto border-2 border-slate-800 mt-2 rounded-lg md:w-[48%]" playsInline autoPlay muted loop >
-                        <source src="/images/pokemon.mp4" type="video/mp4" />
+                        <source src="/images/beluga.mp4" type="video/mp4" />
                     </video>
                 </div>
-                <video className="max-w-[90%] mx-auto border-2 border-slate-800 mt-2 rounded-lg md:w-[48%]" playsInline autoPlay muted loop >
-                    <source src="/images/pokemon.mp4" type="video/mp4" />
-                </video>
+            
                 <video className="max-w-[90%] mx-auto border-2 border-slate-800 mt-2 rounded-lg md:w-[48%]" playsInline autoPlay muted loop >
                     <source src="/images/beluga.mp4" type="video/mp4" />
                 </video>
