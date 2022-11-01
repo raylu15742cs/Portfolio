be651119c8443a8fb1c9aa55c7e313aac40bca75
raylu15742cs <raylucs15742@gmail.com>
2022-11-01 09:19:12 -0700

    fix new sizing

--- portfolio/components/projectcomponent.tsx
+++ portfolio/components/projectcomponent.tsx
@@ -3,13 +3,13 @@ export default function Projectcomponent() {
          <div className=" text-center pt-4 lg:mx-[5%]">
             <h1 className='text-3xl'>Most Recent Projects</h1>
             <div className="flex flex-wrap">
-                <div>
+                <div className="max-w-[90%] mx-auto mt-2 rounded-lg md:w-[48%]">
                     <h1 className='text-3xl'>My Youtube Channel</h1>
                     <video className="max-w-[90%] mx-auto border-2 border-slate-800 mt-2 rounded-lg md:w-[48%]" playsInline autoPlay muted loop >
                         <source src="/images/battleship.mp4" type="video/mp4" />
                     </video>
                 </div>
-                <div>
+                <div className="max-w-[90%] mx-auto  mt-2 rounded-lg md:w-[48%]">
                     <h1 className='text-3xl'>Pokemon Memory Game</h1>
                     <video className="max-w-[90%] mx-auto border-2 border-slate-800 mt-2 rounded-lg md:w-[48%]" playsInline autoPlay muted loop >
                         <source src="/images/pokemon.mp4" type="video/mp4" />
