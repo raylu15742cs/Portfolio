5ce42c933a8a8821c8e01188311272b4a5dfb03c
raylu15742cs <raylucs15742@gmail.com>
2022-11-01 09:48:18 -0700

    add link to pokemon

--- portfolio/components/projectcomponent.tsx
+++ portfolio/components/projectcomponent.tsx
@@ -13,9 +13,11 @@ export default function Projectcomponent() {
                 </div>
                 <div className="max-w-[90%] mx-auto mt-2 rounded-lg md:w-[48%] min-[2500px]:w-[24%]">
                     <h1 className='text-3xl'><a href="https://raymond.is-a.dev/Pokemon-Memory-Game/" target="#"> Pokemon Memory Game </a></h1>
-                    <video className=" border-2 border-slate-800 mt-2 rounded-lg" playsInline autoPlay muted loop >
-                        <source src="/images/pokemon.mp4" type="video/mp4" />
-                    </video>
+                    <a href="https://www.youtube.com/channel/UCZ0lcRhWFgwx0x7qsR-BlsQ" target="#">
+                        <video className=" border-2 border-slate-800 mt-2 rounded-lg" playsInline autoPlay muted loop >
+                            <source src="/images/pokemon.mp4" type="video/mp4" />
+                        </video>
+                    </a>
                 </div>
                 <div className="max-w-[90%] mx-auto mt-2 rounded-lg md:w-[48%] min-[2500px]:w-[24%]">
                     <h1 className='text-3xl'><a href="https://raymond.is-a.dev/Beluga-Theme-Store/" target="#"> Yeezy Beluga Theme Store </a></h1>
