1a8c41c0cf87bde23356fd490da098a0d5d2b87e
raylu15742cs <raylucs15742@gmail.com>
2022-11-01 09:43:41 -0700

    add youtube link

--- portfolio/components/projectcomponent.tsx
+++ portfolio/components/projectcomponent.tsx
@@ -3,7 +3,7 @@ export default function Projectcomponent() {
          <div className=" text-center pt-4 lg:mx-[5%]">
             <div className="flex flex-wrap">
                 <div className="max-w-[90%] mx-auto mt-2 rounded-lg md:w-[48%] min-[2500px]:w-[24%]">
-                    <h1 className='text-3xl'>My Youtube Channel</h1>
+                    <h1 className='text-3xl'><a href="https://www.youtube.com/channel/UCZ0lcRhWFgwx0x7qsR-BlsQ" target="#"> My Youtube Channel </a></h1>
                     <video className=" border-2 border-slate-800 mt-2 rounded-lg" playsInline autoPlay muted loop >
                         <source src="/images/youtube.mp4" type="video/mp4" />
                     </video>
