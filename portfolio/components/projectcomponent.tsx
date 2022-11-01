1648d6f61c24845807fff8e01847a0b872a02422
raylu15742cs <raylucs15742@gmail.com>
2022-11-01 09:20:50 -0700

    fix new sizing

--- portfolio/components/projectcomponent.tsx
+++ portfolio/components/projectcomponent.tsx
@@ -23,7 +23,7 @@ export default function Projectcomponent() {
                 </div>
                 <div>
                     <h1 className='text-3xl'>Battleship Replica</h1>
-                    <video className="max-w-[90%] mx-auto border-2 border-slate-800 mt-2 rounded-lg md:w-[48%]" playsInline autoPlay muted loop >
+                    <video className=" border-2 border-slate-800 mt-2 rounded-lg" playsInline autoPlay muted loop >
                         <source src="/images/battleship.mp4" type="video/mp4" />
                     </video>
                 </div>
