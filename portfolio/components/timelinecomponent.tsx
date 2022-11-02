2f468a5b6e3e08882c8c8f9f2bb7247c9ce9fea9
raylu15742cs <raylucs15742@gmail.com>
2022-11-01 18:33:17 -0700

    update timeline

--- portfolio/components/timelinecomponent.tsx
+++ portfolio/components/timelinecomponent.tsx
@@ -10,8 +10,8 @@ export default function Timelinecomponent() {
                     <h1>2</h1>
                 </div>
                 <div className=" my-auto ml-5 text-xs">
-                    <p>Started Developer Journey on The Odin Project</p>
-                    <p>Created a Youtube Channel </p>
+                    <p>Decided to pursue a career in web development and started working on The Odin Project</p>
+                    <p>Created a Youtube Channel to document my journey as a self taught developer</p>
                 </div>
             </div>
             <div className="flex p-2 mx-[5%] mt-2 border-2 border-slate-900 rounded-lg">
@@ -22,8 +22,8 @@ export default function Timelinecomponent() {
                     <h1>1</h1>
                 </div>
                 <div className=" my-auto ml-5  text-xs">
-                    <p>Awarded Bachelors of Arts in Economics</p>
-                    <p>Took an Introduction to Computer Science Course</p>
+                    <p>Completed Bachelors Degrees in Economics from the University of California Irvine</p>
+                    <p>Sparked my interest in the field of web development with an Introduction to Computer Science Course</p>
                 </div>
             </div>
             <div className="  flex p-2 mx-[5%] mt-2 border-2 border-slate-900 rounded-lg
@@ -36,6 +36,7 @@ export default function Timelinecomponent() {
                 </div>
                 <div className=" my-auto ml-5  text-xs">
                     <p>Completed associates degree and transferred to University of California Irvine</p>
+                    <p>Completed a summer internship at Moon Realty and acquired a real estate license</p>
                 </div>
             </div>
             <div className=" flex p-2 mx-[5%] mt-2 border-2 border-slate-900 rounded-lg">
@@ -47,6 +48,7 @@ export default function Timelinecomponent() {
                 </div>
                 <div className=" my-auto ml-5  text-xs">
                     <p>As a Sophomore decided to take exit exam to start attending college</p>
+                    <p>Competed at the varsity level for the basketball team and became Almont League Champions</p>
                 </div>
             </div>
         </div>
