234a2c340d11c25d80ee8372cfd7dd34d6b254de
raylu15742cs <raylucs15742@gmail.com>
2022-11-01 19:06:07 -0700

    add padding to fun facts

--- portfolio/components/projectcomponent.tsx
+++ portfolio/components/projectcomponent.tsx
@@ -9,7 +9,7 @@ export default function Projectcomponent() {
                             <source src="/images/youtube.mp4" type="video/mp4" />
                         </video>
                     </a>
-                    <p> &#129391; My favorite dish to make is cold smoked salmon since I can give my bagel a huge portion of that delicious goodness &#129391;</p>
+                    <p className="pt-2"> &#129391; My favorite dish to make is cold smoked salmon since I can give my bagel a huge portion of that delicious goodness &#129391;</p>
                 </div>
                 <div className="max-w-[90%] mx-auto mt-2 rounded-lg md:w-[48%] min-[2500px]:w-[24%]">
                     <h1 className='text-3xl'><a href="https://raymond.is-a.dev/Pokemon-Memory-Game/" target="#"> Pokemon Memory Game </a></h1>
@@ -18,7 +18,7 @@ export default function Projectcomponent() {
                             <source src="/images/pokemon.mp4" type="video/mp4" />
                         </video>
                     </a>
-                    <p> 🐕 Tiger, my golden retriever, and I enjoy our daily walks around the neighborhood 🐕</p>
+                    <p className="pt-2"> 🐕 Tiger, my golden retriever, and I enjoy our daily walks around the neighborhood 🐕</p>
                 </div>
                 <div className="max-w-[90%] mx-auto mt-2 rounded-lg md:w-[48%] min-[2500px]:w-[24%]">
                     <h1 className='text-3xl'><a href="https://raymond.is-a.dev/Beluga-Theme-Store/" target="#"> Yeezy Beluga Theme Store </a></h1>
@@ -27,7 +27,7 @@ export default function Projectcomponent() {
                             <source src="/images/beluga.mp4" type="video/mp4" />
                         </video>
                     </a>
-                    <p> &#127936; I try to start my day with a workout plus the swish sound that a basketball makes is music to my ears &#127936;</p>
+                    <p className="pt-2"> &#127936; I try to start my day with a workout plus the swish sound that a basketball makes is music to my ears &#127936;</p>
                 </div>
                 <div className="max-w-[90%] mx-auto mt-2 rounded-lg md:w-[48%] min-[2500px]:w-[24%]">
                     <h1 className='text-3xl'><a href="https://raymond.is-a.dev/Battleship-Game/" target="#"> Battleship </a></h1>
@@ -36,7 +36,7 @@ export default function Projectcomponent() {
                             <source src="/images/battleship.mp4" type="video/mp4" />
                         </video>
                     </a>
-                    <p>🛹 I find that longboarding at the beach or in the park gives me peace of mind as the wind blows by the face 🛹</p>
+                    <p className="pt-2">🛹 I find that longboarding at the beach or in the park gives me peace of mind as the wind blows by the face 🛹</p>
                 </div>
             </div>
         </div>
