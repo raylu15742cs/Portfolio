04af4488d0d3cc60395d8b05edbd486c67c8e59e
raylu15742cs <raylucs15742@gmail.com>
2022-11-01 19:03:48 -0700

    add fun facts

--- portfolio/components/projectcomponent.tsx
+++ portfolio/components/projectcomponent.tsx
@@ -9,7 +9,7 @@ export default function Projectcomponent() {
                             <source src="/images/youtube.mp4" type="video/mp4" />
                         </video>
                     </a>
-                    
+                    <p> &#129391; My favorite dish to make is cold smoked salmon since I can give my bagel a huge portion of that delicious goodness &#129391;</p>
                 </div>
                 <div className="max-w-[90%] mx-auto mt-2 rounded-lg md:w-[48%] min-[2500px]:w-[24%]">
                     <h1 className='text-3xl'><a href="https://raymond.is-a.dev/Pokemon-Memory-Game/" target="#"> Pokemon Memory Game </a></h1>
@@ -18,6 +18,7 @@ export default function Projectcomponent() {
                             <source src="/images/pokemon.mp4" type="video/mp4" />
                         </video>
                     </a>
+                    <p> 🐕 Tiger, my golden retriever, and I enjoy our daily walks around the neighborhood 🐕</p>
                 </div>
                 <div className="max-w-[90%] mx-auto mt-2 rounded-lg md:w-[48%] min-[2500px]:w-[24%]">
                     <h1 className='text-3xl'><a href="https://raymond.is-a.dev/Beluga-Theme-Store/" target="#"> Yeezy Beluga Theme Store </a></h1>
@@ -26,6 +27,7 @@ export default function Projectcomponent() {
                             <source src="/images/beluga.mp4" type="video/mp4" />
                         </video>
                     </a>
+                    <p> &#127936; I try to start my day with a workout plus the swish sound that a basketball makes is music to my ears &#127936;</p>
                 </div>
                 <div className="max-w-[90%] mx-auto mt-2 rounded-lg md:w-[48%] min-[2500px]:w-[24%]">
                     <h1 className='text-3xl'><a href="https://raymond.is-a.dev/Battleship-Game/" target="#"> Battleship </a></h1>
@@ -34,6 +36,7 @@ export default function Projectcomponent() {
                             <source src="/images/battleship.mp4" type="video/mp4" />
                         </video>
                     </a>
+                    <p>🛹 I find that longboarding at the beach or in the park gives me peace of mind as the wind blows by the face 🛹</p>
                 </div>
             </div>
         </div>
--- portfolio/components/timelinecomponent.tsx
+++ portfolio/components/timelinecomponent.tsx
@@ -9,7 +9,8 @@ export default function Timelinecomponent() {
                     <h1>2</h1>
                     <h1>2</h1>
                 </div>
-                <div className=" my-auto ml-5 text-xs">
+                <div className=" my-auto ml-5 min-w-[90%] text-xs">
+                    <p className="font-bold text-center">The Odin Project</p>
                     <p>Decided to pursue a career in web development and started working on The Odin Project</p>
                     <p>Created a Youtube Channel to document my journey as a self taught developer</p>
                 </div>
@@ -21,7 +22,8 @@ export default function Timelinecomponent() {
                     <h1>2</h1>
                     <h1>1</h1>
                 </div>
-                <div className=" my-auto ml-5  text-xs">
+                <div className=" my-auto ml-5 min-w-[90%] text-xs">
+                    <p className="font-bold text-center">University of California Irvine</p>
                     <p>Completed Bachelors Degrees in Economics from the University of California Irvine</p>
                     <p>Sparked my interest in the field of web development with an Introduction to Computer Science Course</p>
                 </div>
@@ -34,8 +36,9 @@ export default function Timelinecomponent() {
                     <h1>1</h1>
                     <h1>9</h1>
                 </div>
-                <div className=" my-auto ml-5  text-xs">
-                    <p>Completed associates degree and transferred to University of California Irvine</p>
+                <div className=" my-auto ml-5 min-w-[90%]  text-xs">
+                    <p className="font-bold text-center">Pasadena City College</p>
+                    <p>Completed associates degree from Pasadena City College and transferred to UCI</p>
                     <p>Completed a summer internship at Moon Realty and acquired a real estate license</p>
                 </div>
             </div>
@@ -46,9 +49,10 @@ export default function Timelinecomponent() {
                     <h1>1</h1>
                     <h1>7</h1>
                 </div>
-                <div className=" my-auto ml-5  text-xs">
+                <div className=" my-auto ml-5  min-w-[90%] text-xs">
+                    <p className="font-bold text-center">San Gabrield High School</p>
                     <p>As a Sophomore decided to take exit exam to start attending college</p>
-                    <p>Competed at the varsity level for the basketball team and became Almont League Champions</p>
+                    <p>Competed in my highs school varsity basketball team and became Almont League Champions</p>
                 </div>
             </div>
         </div>
