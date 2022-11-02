da2b82a295350b56bfa56054598cbeecffb931df
raylu15742cs <raylucs15742@gmail.com>
2022-11-01 18:18:15 -0700

    add more to about me

--- portfolio/components/aboutmecomponent.tsx
+++ portfolio/components/aboutmecomponent.tsx
@@ -5,22 +5,24 @@ export default function Aboutmecomponent() {
             <div className="">
                 <p>Hello! My name is Raymond and I am trying to build a career in the world of web development. I have an economics background, which I value since it taught me to see the world in a logical way.</p>
                 <p className="pt-1">My first taste of web development was when I decided to take an intro to computer science course based on python. It was one of the courses that I will never forget, and it definitely peaked my interest in the field. </p>
-                <p className="pt-1">I decided to take the next step and start learning web development on my own. I started with the Odin Project, and I am currently working on the curriculum. I am also working on a Youtube channel where I will be posting videos on my journey. </p>
-            <p className="pt-1">Here are a few technologies that I currently am using: </p>
-            <div className="flex pt-1">
-                <ul className="pr-8 pl-7 list-disc">
-                    <li>JavaScript</li>
-                    <li>React</li>
-                </ul>
-                <ul className="list-disc pr-8">
-                    <li>TypeScript</li>
-                    <li>Node.js</li>
-                </ul>
-                <ul className="list-disc">
-                    <li>Next.js</li>
-                    <li>Tailwind</li>
-                </ul>
-            </div>
+                <p className="pt-1">I decided to take the next step and start learning web development on my own. I started with the Odin Project, and I am currently working on the curriculum. I am also working on my Youtube channel where I will be posting videos on my journey. </p>
+                <p className="pt-1">My goal is to become a front end developer, and I am currently working on my skills in React, Node, and Python. I am also working on my skills in UI/UX design. </p>
+                <p className="pt-1">I am currently looking for a job as a junior developer, and I am open to any opportunities. I am also open to any advice or tips that you may have for me. </p>
+                <p className="pt-1">Here are a few technologies that I currently am using: </p>
+                <div className="flex pt-1">
+                    <ul className="pr-8 pl-7 list-disc">
+                        <li>JavaScript</li>
+                        <li>React</li>
+                    </ul>
+                    <ul className="list-disc pr-8">
+                        <li>TypeScript</li>
+                        <li>Node.js</li>
+                    </ul>
+                    <ul className="list-disc">
+                        <li>Next.js</li>
+                        <li>Tailwind</li>
+                    </ul>
+                </div>
             </div>
         </div>
     )
