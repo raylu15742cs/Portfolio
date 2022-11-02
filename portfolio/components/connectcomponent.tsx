ff17772f253358c06ad778151fefa7195df077a7
raylu15742cs <raylucs15742@gmail.com>
2022-11-02 08:38:34 -0700

    update connect

--- portfolio/components/connectcomponent.tsx
+++ portfolio/components/connectcomponent.tsx
@@ -16,7 +16,7 @@ export default function Connectcomponent() {
                     <Image className="h-[30px] w-auto  " src="/images/youtubeicon.png" alt={''} width={500} height={500} />
                 </a>
                  <a className="mx-auto"  href="https://github.com/raylu15742cs" target="#">
-                    <Image className="h-[40px] w-auto" src="/images/githubicon.png" alt={''} width={500} height={500} />
+                    <Image className="h-[30px] w-auto" src="/images/githubicon.png" alt={''} width={500} height={500} />
                 </a>
                 <a className="mx-auto"  href="https://twitter.com/RaymondLuTech" target="#">
                     <Image className="h-[45px] w-auto" src="/images/twittericon.png" alt={''} width={500} height={500} />
