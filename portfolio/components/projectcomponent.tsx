79f02cd03a4f6e73c42859553222b619d9c5d1ec
raylu15742cs <raylucs15742@gmail.com>
2023-03-07 13:42:23 -0800

    fix error

--- portfolio/components/projectcomponent.tsx
+++ portfolio/components/projectcomponent.tsx
@@ -11,16 +11,22 @@ export default function Projectcomponent() {
                 </div>
                 <div className="max-w-[90%] mx-auto mt-2 rounded-lg md:w-[48%] min-[2500px]:w-[24%]">
                     <h1 className='text-3xl'><a href="https://studibetterfront.onrender.com/" target="#"> StudiBetter </a></h1>
+                    <h1>Description</h1>
+                    <p>StudiBetter is an application for students and learners that target their weakness. By using a targeted quiz system, users get specialized quizs that improve their performance and memorization.</p>
+                    <p>Struggles</p>
+                    <p>Latest Feature</p>
+                    <p>Future Plans</p>
+                    <a className="pr-2 font-medium text-blue-600 dark:text-blue-500 hover:underline" href="https://studibetterfront.onrender.com/">Live Link</a>
+                    <a className="font-medium text-blue-600 dark:text-blue-500 hover:underline" href="https://github.com/raylu15742cs/studibetter2.0">Github Repo</a>
                 </div>
                 <div className="max-w-[90%] mx-auto mt-2 rounded-lg md:w-[48%] min-[2500px]:w-[24%]">
-                    <h1 className='text-3xl'><a href="https://raymond.is-a.dev/Pokemon-Memory-Game/" target="#"> Pokemon Memory Game </a></h1>
-                </div>
-                <div className="max-w-[90%] mx-auto mt-2 rounded-lg md:w-[48%] min-[2500px]:w-[24%]">
-                    <a href="https://raymond.is-a.dev/Pokemon-Memory-Game/" target="#">
-                        <video className=" border-2 border-slate-800 mt-2 rounded-lg" playsInline autoPlay muted loop >
-                            <source src="/images/pokemon.mp4" type="video/mp4" />
-                        </video>
-                    </a>
+                    <h1 className='text-3xl'><a href="https://raymond.is-a.dev/Beluga-Theme-Store/" target="#"> Yeezy Beluga Theme Store </a></h1>
+                    <p>Description</p>
+                    <p>Struggles</p>
+                    <p>Latest Feature</p>
+                    <p>Future Plans</p>
+                    <a className="pr-2 font-medium text-blue-600 dark:text-blue-500 hover:underline"  href="https://raylu15742cs.github.io/Beluga-Theme-Store/">Live Link</a>
+                    <a className="font-medium text-blue-600 dark:text-blue-500 hover:underline" href="https://github.com/raylu15742cs/Beluga-Theme-Store">Github Repo</a>
                 </div>
                 <div className="max-w-[90%] mx-auto mt-2 rounded-lg md:w-[48%] min-[2500px]:w-[24%]">
                     <a href="https://raymond.is-a.dev/Beluga-Theme-Store/" target="#">
@@ -30,7 +36,20 @@ export default function Projectcomponent() {
                     </a>
                 </div>
                 <div className="max-w-[90%] mx-auto mt-2 rounded-lg md:w-[48%] min-[2500px]:w-[24%]">
-                    <h1 className='text-3xl'><a href="https://raymond.is-a.dev/Beluga-Theme-Store/" target="#"> Yeezy Beluga Theme Store </a></h1>
+                    <a href="https://raymond.is-a.dev/Pokemon-Memory-Game/" target="#">
+                        <video className=" border-2 border-slate-800 mt-2 rounded-lg" playsInline autoPlay muted loop >
+                            <source src="/images/pokemon.mp4" type="video/mp4" />
+                        </video>
+                    </a>
+                </div>
+                <div className="max-w-[90%] mx-auto mt-2 rounded-lg md:w-[48%] min-[2500px]:w-[24%]">
+                    <h1 className='text-3xl'><a href="https://raymond.is-a.dev/Pokemon-Memory-Game/" target="#"> Pokemon Memory Game </a></h1>
+                    <p>Description</p>
+                    <p>Struggles</p>
+                    <p>Latest Feature</p>
+                    <p>Future Plans</p>
+                    <a className="pr-2 font-medium text-blue-600 dark:text-blue-500 hover:underline" href="https://raymond.is-a.dev/Pokemon-Memory-Game/">Live Link</a>
+                    <a className="font-medium text-blue-600 dark:text-blue-500 hover:underline"  href="https://github.com/raylu15742cs/Pokemon-Memory-Game">Github Repo</a>
                 </div>
             </div>
         </div>
